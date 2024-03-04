2a7fa144fa92f810cb6477a099de0aaf4d705523
darko-glitch <42360427+darko-glitch@users.noreply.github.com>
2024-03-04 18:49:14 +0100

    Add files via upload

---
+++ before.sh
@@ -0,0 +1,4 @@
+sudo dnf install -y dnf-plugins-core zsh zsh-autosuggestions @base-x gnome-extensions-app zsh zsh-autosuggestions gnome-browser-connector gnome-shell gnome-terminal dnf-plugins-core gnome-calculator gnome-system-monitor gedit file-roller nautilus gnome-tweaks @development-tools
+
+sudo systemctl set-default graphical.target
+
