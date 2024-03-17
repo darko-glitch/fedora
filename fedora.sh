13e87a42ebf4c85f9405112dd10daafd32227d29
darko-glitch <42360427+darko-glitch@users.noreply.github.com>
2024-03-17 17:39:02 +0100

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -89,8 +89,8 @@ sudo ufw default allow outgoing
 sudo ufw enable
 
 # Install Miniconda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p $HOME/miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/Downloads/miniconda.sh
+bash ~/Downloads/miniconda.sh -b -p $HOME/miniconda
 eval "$($HOME/miniconda/bin/conda shell.bash hook)"
 conda init
 
